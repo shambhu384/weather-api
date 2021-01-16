343301a05396e541360dc0d8fa397364617e2f69
Shambhu Kumar <shambhu384@gmail.com>
2021-01-17 03:14:30 +0530

    Added nelmio doc

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
